9fbcb01b1122314d0c318385cc80570c03389521
TheAliBigdeli <bigdeli.ali3@gmail.com>
2024-07-16 02:45:31 +0330

    Update main.py

--- core/main.py
+++ core/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Query,status,HTTPException
+from fastapi.responses import JSONResponse
+import random
 
 app = FastAPI()
 
@@ -9,12 +10,22 @@ names_list = [
     {"id":3, "name":"arousha"},
     {"id":4, "name":"aziz"},
     {"id":5, "name":"zahra"},
+    {"id":6, "name":"ali"},
+    {"id":7, "name":"ali"},
 ]
 
 # /names (GET(RETRIEVE),POST(CREATE))
 @app.get("/names")
-def retrieve_names_list():
+def retrieve_names_list(q : str | None = Query(default=None, max_length=50)):
+    if q:
+        return [item for item in names_list if item["name"] == q ]
     return names_list
+
+@app.post("/names",status_code=status.HTTP_201_CREATED)
+def create_name(name:str):
+    name_obj = {"id":random.randint(6,100),"name":name}
+    names_list.append(name_obj)
+    return name_obj
 
 
 # /names/:id (GET(RETRIEVE),PUT/PATCH(UPDATE),DELETE)
@@ -23,10 +34,27 @@ def retrieve_name_detail(name_id:int):
     for name in names_list:
         if name["id"] == name_id:
             return name
-    return {"detail":"object not found"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
 
+@app.put("/names/{name_id}",status_code=status.HTTP_200_OK)
+def update_name_detail(name_id:int,name:str):
+    for item in names_list:
+        if item["id"] == name_id:
+            item["name"] = name
+            return item
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
+
+@app.delete("/names/{name_id}")
+def delete_name(name_id:int):
+    for item in names_list:
+        if item["id"] == name_id:
+            names_list.remove(item)
+            return JSONResponse(content={"detail":"object removed successfully"},status_code=status.HTTP_200_OK)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
+            
 
 @app.get("/")
 def root():
-    return {"message":"Hello World! "}
+    content = {"message":"Hello World! "}
+    return JSONResponse(content=content,status_code=status.HTTP_202_ACCEPTED)
 
