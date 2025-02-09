983da61b31b22ad97d4edc16b110894c621e8588
TheAliBigdeli <bigdeli.ali3@gmail.com>
2025-02-09 09:40:06 +0330

    added cors middleware

--- core/main.py
+++ core/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,Request
 from contextlib import asynccontextmanager
 from tasks.routes import router as tasks_routes
 from users.routes import router as users_routes
+import time
+from fastapi.middleware.cors import CORSMiddleware
+
 
 tags_metadata = [
     {
@@ -43,3 +46,25 @@ app = FastAPI(
 
 app.include_router(tasks_routes)
 app.include_router(users_routes)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+origins = [
+    "http://127.0.0.1:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
